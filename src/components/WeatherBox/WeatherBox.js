8e8f68d4e34b4e15f5b51a4f67557c0be08af7bb
Michał Wiaderkowicz <michal.wiaderkowicz@onet.pl>
2024-03-20 14:52:55 +0100

    add API key to weatherBox

--- src/components/WeatherBox/WeatherBox.js
+++ src/components/WeatherBox/WeatherBox.js
@@ -5,7 +5,13 @@ import { useCallback } from "react";
 
 const WeatherBox = (props) => {
   const handleCityChange = useCallback((city) => {
-    console.log("city:", city);
+    fetch(
+      `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=db0acb1792004fc9c27cbedbddb090d1&units=metric`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+      });
   }, []);
   return (
     <section>

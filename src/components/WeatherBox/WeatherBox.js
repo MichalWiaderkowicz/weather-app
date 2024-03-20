ca21ab75858119a77b6713d5d2301a4f94995a8f
Michał Wiaderkowicz <michal.wiaderkowicz@onet.pl>
2024-03-20 16:22:04 +0100

    Use weatherData inside WeatherSummary

--- src/components/WeatherBox/WeatherBox.js
+++ src/components/WeatherBox/WeatherBox.js
@@ -1,22 +1,29 @@
 import PickCity from "../PickCity/PickCity";
 import WeatherSummary from "../WeatherSummary/WeatherSummary";
 import Loader from "../Loader/Loader";
-import { useCallback } from "react";
+import { useCallback, useState } from "react";
 
 const WeatherBox = (props) => {
+  const [weatherData, setWeatherData] = useState("");
   const handleCityChange = useCallback((city) => {
     fetch(
       `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=db0acb1792004fc9c27cbedbddb090d1&units=metric`
     )
       .then((res) => res.json())
       .then((data) => {
-        console.log(data);
+        setWeatherData({
+          city: data.name,
+          temp: data.main.temp,
+          icon: data.weather[0].icon,
+          description: data.weather[0].main,
+        });
       });
   }, []);
+  console.log(weatherData);
   return (
     <section>
       <PickCity action={handleCityChange} />
-      <WeatherSummary />
+      <WeatherSummary weatherData={weatherData} />
       <Loader />
     </section>
   );
